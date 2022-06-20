cf08f89746b56b1eb424abf0983c5ebe1c5a4f1f
clopesbraga <clopesbraga@gmail.com>
2022-06-19 21:58:13 -0300

    Subida de testes.

---
+++ app/src/test/java/com/example/afimdefeirax/LoginTest.kt
@@ -0,0 +1,21 @@
+package com.example.afimdefeirax
+import com.example.afimdefeirax.Repository.LoginRepository
+import org.junit.Assert
+import org.junit.Test
+
+
+class LoginTest {
+
+    @Test
+    fun conexaoBancoTest(){
+
+
+    }
+
+    @Test
+    fun registrousuarioTest(){
+
+
+    }
+
+}
