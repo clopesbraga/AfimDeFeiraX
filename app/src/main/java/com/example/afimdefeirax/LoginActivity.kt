2eea1f59c0f0ea00ba78aae78eec089278a9229b
clopesbraga <clopesbraga@gmail.com>
2022-06-26 21:00:24 -0300

    feature(Login) criada Tela LoginActivity.

---
+++ app/src/main/java/com/example/afimdefeirax/LoginActivity.kt
@@ -0,0 +1,44 @@
+package com.example.afimdefeirax
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import androidx.lifecycle.ViewModelProvider
+import com.example.afimdefeirax.ViewModel.LoginViewModel
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var viewmodel : LoginViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        viewmodel=ViewModelProvider(this).get(LoginViewModel::class.java)
+
+
+    }
+      fun onClick(v:View){
+
+        val id= v.id
+
+        if(id==R.id.btn_salvar){
+
+            val usuario = findViewById<EditText>(R.id.edt_email)
+            val nome = findViewById<EditText>(R.id.edt_nome)
+            val senha = findViewById<EditText>(R.id.edt_senha)
+
+            viewmodel.login(
+                id,
+                usuario.text.toString(),
+                nome.text.toString(),
+                senha.text.toString()
+            )
+
+        }
+
+    }
+
+}
