f0b30b33325a3866646650b4220bcaf80f0093ea
Cleiton <clopesbraga@gmail.com>
2022-08-28 19:10:55 -0300

    fix(login):incluido método observe() para verificar retorno do método mSaveLogin

--- app/src/main/java/com/example/afimdefeirax/LoginActivity.kt
+++ app/src/main/java/com/example/afimdefeirax/LoginActivity.kt
@@ -8,19 +8,21 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.afimdefeirax.ViewModel.LoginViewModel
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var viewmodel: LoginViewModel
+    private var idUsuario : Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
         viewmodel = ViewModelProvider(this).get(LoginViewModel::class.java)
-
 
     }
 
@@ -35,13 +37,25 @@ class LoginActivity : AppCompatActivity() {
             val senha = findViewById<EditText>(R.id.edt_senha)
 
             viewmodel.login(
-                id,
+                idUsuario,
                 usuario.text.toString(),
                 nome.text.toString(),
                 senha.text.toString()
             )
-
         }
+        observe()
+    }
+
+    private fun observe(){
+
+        viewmodel.mSaveLogin.observe(this, Observer {
+
+            if(it){
+                Toast.makeText(applicationContext, "Sucesso", Toast.LENGTH_LONG).show()
+            }else {
+                Toast.makeText(applicationContext, "Falha", Toast.LENGTH_LONG).show()
+            }
+        })
 
     }
 
