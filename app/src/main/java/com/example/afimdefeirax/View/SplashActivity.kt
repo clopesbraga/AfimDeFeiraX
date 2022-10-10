388e77f665ebbc96963628717d888176c1af5b60
Cleiton <clopesbraga@gmail.com>
2022-10-09 23:00:59 -0300

    fix(Tela Splash): incluido tela SplashActivity.kt.

---
+++ app/src/main/java/com/example/afimdefeirax/View/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.example.afimdefeirax.View
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.afimdefeirax.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
