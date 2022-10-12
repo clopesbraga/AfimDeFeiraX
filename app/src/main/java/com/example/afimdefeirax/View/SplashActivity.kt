368003168fe3e51a5cbe0e6a83b18bdd24d70cab
Cleiton <clopesbraga@gmail.com>
2022-10-12 18:22:39 -0300

    fix(SplashActivity):configurado opção para saber se id já foi cadastrado ou não.

--- app/src/main/java/com/example/afimdefeirax/View/SplashActivity.kt
+++ app/src/main/java/com/example/afimdefeirax/View/SplashActivity.kt
@@ -1,12 +1,41 @@
 package com.example.afimdefeirax.View
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
 import com.example.afimdefeirax.R
+import com.example.afimdefeirax.SharedPreferences.LoginShared
+import kotlinx.coroutines.CoroutineStart
+import kotlinx.coroutines.delay
+import kotlin.time.measureTime
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var msharedlogin: LoginShared
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        msharedlogin = LoginShared(application.applicationContext)
+        inicializeMainActivity()
+
+    }
+
+    fun inicializeMainActivity(){
+        Handler().postDelayed({
+            verifyAcess()
+            finish()
+        }, 3000)
+    }
+
+    private fun verifyAcess(){
+        if (!msharedlogin.getString("id").isNullOrEmpty()){
+            startActivity( Intent(this,MainActivity::class.java))
+        }else{
+            startActivity( Intent(this,LoginActivity::class.java))
+        }
     }
 }
