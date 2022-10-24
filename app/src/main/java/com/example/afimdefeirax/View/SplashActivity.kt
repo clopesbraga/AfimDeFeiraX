abc635dd0cf42788137b4fd1c4eeb9380b2606e9
Cleiton <clopesbraga@gmail.com>
2022-10-23 22:21:41 -0300

    fix(Splash): criada segunda tela SplashActivity2.kt

--- app/src/main/java/com/example/afimdefeirax/View/SplashActivity.kt
+++ app/src/main/java/com/example/afimdefeirax/View/SplashActivity.kt
@@ -4,13 +4,10 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import com.example.afimdefeirax.R
-import com.example.afimdefeirax.SharedPreferences.LoginShared
 import com.example.afimdefeirax.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
 
-    private lateinit var msharedlogin: LoginShared
     private lateinit var binding: ActivitySplashBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,30 +16,19 @@ class SplashActivity : AppCompatActivity() {
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        msharedlogin = LoginShared(application.applicationContext)
         inicializeMainActivity()
     }
 
     private fun inicializeMainActivity() {
         Handler().postDelayed({
             changeImageSplash()
-            verifyAcess()
             finish()
-        }, 3000)
+        }, 2500)
     }
 
     private fun changeImageSplash() {
-        binding.imgLogoBackground.setImageResource(R.mipmap.ic_empresa_logo_background)
-        binding.imgLogoBackground.setImageResource(R.mipmap.ic_logo_app_foreground)
-    }
 
-    private fun verifyAcess() {
-        if (!msharedlogin.getString("id").isNullOrEmpty()) {
-            startActivity(Intent(this, MainActivity::class.java))
-        } else {
-            startActivity(Intent(this, LoginActivity::class.java))
-        }
+        startActivity(Intent(this, SplashActivity2::class.java))
     }
-
 
 }
