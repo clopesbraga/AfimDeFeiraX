e487f1e7a6e6bb3fb26e2df866e78d62f39190ca
Cleiton Lopes Braga <clopesbraga@gmail.com>
2023-04-28 11:33:02 -0300

    fix(Menu):ajustes feito na MainActivity.ktctivity,activity_main_drawer.xml e mobile_navigation.xml

--- app/src/main/java/com/example/afimdefeirax/View/MainActivity.kt
+++ app/src/main/java/com/example/afimdefeirax/View/MainActivity.kt
@@ -38,7 +38,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,R.id.nav_compras
+                R.id.nav_map_feiras, R.id.nav_compras, R.id.nav_historico
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
