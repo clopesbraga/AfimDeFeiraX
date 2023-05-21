3d7d7bbaa35f3d6280d4ec8acc2936400774d332
Cleiton Lopes Braga <clopesbraga@gmail.com>
2023-05-20 23:28:02 -0300

    feature(produtos):reformulação da tela de lista de produtos.

--- app/src/main/java/com/example/afimdefeirax/View/ComprasFragment.kt
+++ app/src/main/java/com/example/afimdefeirax/View/ComprasFragment.kt
@@ -2,16 +2,18 @@ package com.example.afimdefeirax.View
 
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.fragment.app.Fragment
 import com.example.afimdefeirax.ViewModel.ComprasViewModel
-
 import com.example.afimdefeirax.databinding.FragmentComprasBinding
+import com.example.afimdefeirax.R
 
 
-class ComprasFragment : Fragment() {
+class ComprasFragment : Fragment(){
 
     companion object {
         fun newInstance() = ComprasFragment()
@@ -19,7 +21,12 @@ class ComprasFragment : Fragment() {
 
     private lateinit var comprasViewModel: ComprasViewModel
     private var _binding: FragmentComprasBinding? = null
-    private val binding get() = _binding!!
+
+    private lateinit var spinnerselecaodacidade: Spinner
+    private lateinit var produtos_adapter: ArrayAdapter<*>
+
+    val binding get() = _binding!!
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,6 +36,17 @@ class ComprasFragment : Fragment() {
 
         _binding = FragmentComprasBinding.inflate(inflater, container, false)
         val root: View = binding.root
+
+        spinnerselecaodacidade = binding.spinVerduras
+
+        produtos_adapter = ArrayAdapter.createFromResource(
+            root.context,
+            R.array.produtos,
+            androidx.transition.R.layout.support_simple_spinner_dropdown_item
+        )
+        spinnerselecaodacidade.setAdapter(produtos_adapter)
+
+
 
         return root
     }
