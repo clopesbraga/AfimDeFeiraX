21a3aed89878adbf4de11556f6941f8e846f4b0f
Cleiton Lopes Braga <clopesbraga@gmail.com>
2023-06-05 23:18:03 -0300

    feature(produtos):configurando classes ComprasFragment.kt e ComprasViewModel.kt

--- app/src/main/java/com/example/afimdefeirax/View/ComprasFragment.kt
+++ app/src/main/java/com/example/afimdefeirax/View/ComprasFragment.kt
@@ -5,9 +5,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.example.afimdefeirax.ViewModel.ComprasViewModel
 import com.example.afimdefeirax.databinding.FragmentComprasBinding
 import com.example.afimdefeirax.R
@@ -19,11 +21,14 @@ class ComprasFragment : Fragment(){
         fun newInstance() = ComprasFragment()
     }
 
-    private lateinit var comprasViewModel: ComprasViewModel
+    private lateinit var _viewModel: ComprasViewModel
     private var _binding: FragmentComprasBinding? = null
 
-    private lateinit var spinnerselecaodacidade: Spinner
-    private lateinit var produtos_adapter: ArrayAdapter<*>
+    private lateinit var _spinnerprodutos: Spinner
+    private lateinit var _produtos_adapter: ArrayAdapter<*>
+
+    private lateinit var adapterview : AdapterView.OnItemClickListener
+
 
     val binding get() = _binding!!
 
@@ -37,23 +42,40 @@ class ComprasFragment : Fragment(){
         _binding = FragmentComprasBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        spinnerselecaodacidade = binding.spinVerduras
+        _viewModel=ViewModelProvider(this).get(ComprasViewModel::class.java)
 
-        produtos_adapter = ArrayAdapter.createFromResource(
+        _spinnerprodutos = binding.spinVerduras
+
+        _produtos_adapter = ArrayAdapter.createFromResource(
             root.context,
             R.array.produtos,
             androidx.transition.R.layout.support_simple_spinner_dropdown_item
         )
-        spinnerselecaodacidade.setAdapter(produtos_adapter)
+        _spinnerprodutos.setAdapter(_produtos_adapter)
 
+        adapterview.onItemClick(){
 
+            when(_spinnerprodutos.selectedItemPosition){
+
+            }
+
+        }
 
         return root
     }
+
+    fun mostrarProdutos(){
+        _viewModel.listaProdutos()
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
+}
+
+private fun AdapterView.OnItemClickListener.onItemClick(function: () -> Unit) {
 
 }
