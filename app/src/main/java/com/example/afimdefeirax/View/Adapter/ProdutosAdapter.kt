3fc40f1bc3f20392b1be7e1763dcbe59e0f48f3a
Cleiton Lopes Braga <clopesbraga@gmail.com>
2023-06-05 23:10:24 -0300

    feature(produtos):inicio da inclusao da recyclerview criando a classe ProdutosAdapter.kt

---
+++ app/src/main/java/com/example/afimdefeirax/View/Adapter/ProdutosAdapter.kt
@@ -0,0 +1,19 @@
+package com.example.afimdefeirax.View.Adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ProdutosAdapter: RecyclerView.Adapter<VH>{
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: VH, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+}
