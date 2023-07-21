a6179a5d43627c54d6b344df095308ce9948eebc
Cleiton Lopes Braga <clopesbraga@gmail.com>
2023-07-20 22:41:12 -0300

    inclusao(Tela-Produtos): subida das classes ProdutosAdapter e ProdutosHolder

--- app/src/main/java/com/example/afimdefeirax/View/Adapter/ProdutosAdapter.kt
+++ app/src/main/java/com/example/afimdefeirax/View/Adapter/ProdutosAdapter.kt
@@ -2,13 +2,14 @@ package com.example.afimdefeirax.View.Adapter
 
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.afimdefeirax.View.Holder.ProdutosHolder
 
-class ProdutosAdapter: RecyclerView.Adapter<VH>{
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH {
+class ProdutosAdapter: RecyclerView.Adapter<ProdutosHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProdutosHolder {
         TODO("Not yet implemented")
     }
 
-    override fun onBindViewHolder(holder: VH, position: Int) {
+    override fun onBindViewHolder(holder: ProdutosHolder, position: Int) {
         TODO("Not yet implemented")
     }
 
