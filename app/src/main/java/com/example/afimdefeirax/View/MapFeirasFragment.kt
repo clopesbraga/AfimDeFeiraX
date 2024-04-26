d715d5b03ceff596bd7296f059718b1fdd7bbf6d
Cleiton Lopes Braga <clopesbraga@gmail.com>
2024-04-25 23:41:49 -0300

    refactor(TelaMapa): mudando as funcoes de focalizar a camera na classe MapFeirasFragment

--- app/src/main/java/com/example/afimdefeirax/View/MapFeirasFragment.kt
+++ app/src/main/java/com/example/afimdefeirax/View/MapFeirasFragment.kt
@@ -1,26 +1,51 @@
 package com.example.afimdefeirax.View
 
+import android.Manifest
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.example.afimdefeirax.Model.FeirasModel
 import com.example.afimdefeirax.R
-import com.example.afimdefeirax.ViewModel.MapaFeirasViewModel
+import com.example.afimdefeirax.Utils.DeviceCurrentTime
 import com.example.afimdefeirax.databinding.FragmentMapFeirasBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
-import org.koin.android.ext.android.inject
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 
 class MapFeirasFragment : Fragment(), OnMapReadyCallback {
 
     private var _binding: FragmentMapFeirasBinding? = null
     private val binding get() = _binding!!
-    private val viewModel: MapaFeirasViewModel by inject()
+//    private val viewModel: MapaFeirasViewModel by inject()
+    private val diasemana = DeviceCurrentTime()
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var userlocalization: LatLng
+    private var database: DatabaseReference = let{
+        Firebase.database.reference.child("Pesquisa").child("Feiras")
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -39,6 +64,13 @@ class MapFeirasFragment : Fragment(), OnMapReadyCallback {
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
         mapFragment?.getMapAsync(this)
 
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(binding.root.context)
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+
     }
 
     override fun onDestroyView() {
@@ -48,9 +80,94 @@ class MapFeirasFragment : Fragment(), OnMapReadyCallback {
 
     override fun onMapReady(map: GoogleMap): Unit = map.let {
 
-        viewModel.showMyLocalization(it, binding.root.context)
-        viewModel.showFeirasLocalization(it,binding.root.context)
+        showMyLocalizationIn(map)
+        showFeirasLocalizationIn(map)
 
+    }
+
+    private fun showMyLocalizationIn(map: GoogleMap) {
+        when {
+            ContextCompat.checkSelfPermission(
+                binding.root.context as Activity,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED -> {
+
+                fusedLocationClient.lastLocation.addOnSuccessListener {
+
+                    if (it != null) {
+
+                        val latitude = it.latitude
+                        val longitude = it.longitude
+                        userlocalization = LatLng(latitude, longitude)
+
+                        val cameraPosition = CameraPosition.Builder()
+                            .target(userlocalization)
+                            .zoom(13f)
+                            .build()
+
+                        map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
+                        map.addMarker(MarkerOptions().position(userlocalization).title("Marker"))
+                    }
+
+                    binding.fabLocalization.setOnClickListener{
+
+                        val cameraPosition = CameraPosition.Builder()
+                            .target(userlocalization)
+                            .zoom(13f)
+                            .build()
+
+                        map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
+
+                    }
+
+                }
+
+            }
+
+            else -> ActivityCompat.requestPermissions(
+                binding.root.context as Activity,
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ),
+                200
+            )
+        }
+    }
+
+    private fun showFeirasLocalizationIn(map: GoogleMap){
+        val referenceListener = object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+                for (feirasnapshot in snapshot.children) {
+
+                    val feiras = feirasnapshot.getValue(FeirasModel::class.java)
+                    feiras?.let {
+                        if(diasemana.trazSemana() == it.dia){
+
+                            val latLng = LatLng(it.Latitude.toDouble(), it.Longitude.toDouble())
+                            map.addMarker(
+                                MarkerOptions()
+                                    .position(latLng)
+                                    .title("FEIRA ${it.Feira}")
+                                    .snippet(it.bairro)
+                                    .contentDescription(it.endereco)
+                                    .icon(
+                                        BitmapDescriptorFactory.fromResource(R.mipmap.ic_loc)
+                                    )
+                            )
+                        }
+                    }
+
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.d("FIREBASE", "Failed to read", error.toException())
+            }
+        }
+
+        database.addListenerForSingleValueEvent(referenceListener)
     }
 
 }
