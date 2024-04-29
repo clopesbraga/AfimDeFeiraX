968249d81d9fb2ca3574fa7cc3c5d27f2ee62af5
Cleiton Lopes Braga <clopesbraga@gmail.com>
2024-04-29 14:22:32 -0300

    refactor(TelaMapa): criando funcoes de busca de feiras e foco no mapa

--- app/src/main/java/com/example/afimdefeirax/View/MapFeirasFragment.kt
+++ app/src/main/java/com/example/afimdefeirax/View/MapFeirasFragment.kt
@@ -4,16 +4,22 @@ import android.Manifest
 import android.app.Activity
 import android.app.AlertDialog
 import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.afimdefeirax.Model.FeirasModel
 import com.example.afimdefeirax.R
+import com.example.afimdefeirax.Utils.Camera
 import com.example.afimdefeirax.Utils.DeviceCurrentTime
 import com.example.afimdefeirax.databinding.FragmentMapFeirasBinding
 import com.google.android.gms.location.FusedLocationProviderClient
@@ -23,7 +29,6 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
-import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.firebase.database.DataSnapshot
@@ -32,17 +37,23 @@ import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import java.io.IOException
+import java.util.Objects
 
 
 class MapFeirasFragment : Fragment(), OnMapReadyCallback {
 
     private var _binding: FragmentMapFeirasBinding? = null
     private val binding get() = _binding!!
-//    private val viewModel: MapaFeirasViewModel by inject()
+
+    //    private val viewModel: MapaFeirasViewModel by inject()
     private val diasemana = DeviceCurrentTime()
+    private val camera = Camera()
+
     private lateinit var fusedLocationClient: FusedLocationProviderClient
     private lateinit var userlocalization: LatLng
-    private var database: DatabaseReference = let{
+
+    private var database: DatabaseReference = let {
         Firebase.database.reference.child("Pesquisa").child("Feiras")
     }
 
@@ -68,11 +79,6 @@ class MapFeirasFragment : Fragment(), OnMapReadyCallback {
 
     }
 
-    override fun onResume() {
-        super.onResume()
-
-    }
-
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
@@ -82,6 +88,133 @@ class MapFeirasFragment : Fragment(), OnMapReadyCallback {
 
         showMyLocalizationIn(map)
         showFeirasLocalizationIn(map)
+
+        binding.fabSearch.setOnClickListener {
+
+            lateinit var bairrosadapter: ArrayAdapter<*>
+            val inflater = requireActivity().layoutInflater
+            val mensagem = AlertDialog.Builder(
+                activity
+            )
+
+            val janelacidadesview =
+                inflater.inflate(R.layout.layout_list_cities_ditricts, null) as ViewGroup
+
+            val spinnerselecaodacidade = janelacidadesview.findViewById<Spinner>(R.id.spinnercidade)
+            val spinnerselecaodobairro = janelacidadesview.findViewById<Spinner>(R.id.spinnerbairro)
+
+
+            val cidadesadapter = ArrayAdapter.createFromResource(
+                janelacidadesview.context,
+                R.array.cidades,
+                android.R.layout.simple_spinner_dropdown_item
+            )
+
+            spinnerselecaodacidade.adapter=cidadesadapter
+
+            val escolha = object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>,
+                    view: View,
+                    position: Int,
+                    id: Long
+                ) {
+
+                    when (spinnerselecaodacidade.selectedItemPosition) {
+                        0 -> {
+                            bairrosadapter = ArrayAdapter.createFromResource(
+                                view.context,
+                                R.array.sp_bairros,
+                                android.R.layout.simple_spinner_dropdown_item
+                            )
+                        }
+
+                        1 -> {
+                            bairrosadapter = ArrayAdapter.createFromResource(
+                                view.context,
+                                R.array.guaru_bairros,
+                                android.R.layout.simple_spinner_dropdown_item
+                            )
+                        }
+
+                        2 -> {
+                            bairrosadapter = ArrayAdapter.createFromResource(
+                                view.context,
+                                R.array.suza_bairros,
+                                android.R.layout.simple_spinner_dropdown_item
+                            )
+                        }
+
+                        3 -> {
+                            bairrosadapter = ArrayAdapter.createFromResource(
+                                view.context,
+                                R.array.osasco_bairros,
+                                android.R.layout.simple_spinner_dropdown_item
+                            )
+                        }
+
+                        4 -> {
+                            bairrosadapter = ArrayAdapter.createFromResource(
+                                view.context,
+                                R.array.maua_bairros,
+                                android.R.layout.simple_spinner_dropdown_item
+                            )
+                        }
+
+                        5 -> {
+                            bairrosadapter = ArrayAdapter.createFromResource(
+                                view.context,
+                                R.array.andre_bairros,
+                                android.R.layout.simple_spinner_dropdown_item
+                            )
+                        }
+
+                        6 -> {
+                            bairrosadapter = ArrayAdapter.createFromResource(
+                                view.context,
+                                R.array.bernado_bairros,
+                                android.R.layout.simple_spinner_dropdown_item
+                            )
+                        }
+
+                        7 -> {
+                            bairrosadapter = ArrayAdapter.createFromResource(
+                                view.context,
+                                R.array.caetano_bairros,
+                                android.R.layout.simple_spinner_dropdown_item
+                            )
+                        }
+
+                        else -> {
+                            bairrosadapter = ArrayAdapter.createFromResource(
+                                view.context,
+                                R.array.sp_bairros,
+                                android.R.layout.simple_spinner_dropdown_item
+                            )
+                        }
+                    }
+
+                    spinnerselecaodobairro.adapter = bairrosadapter
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>) {
+                    // Implementação opcional caso deseje fazer algo quando nada for selecionado
+                }
+
+            }
+
+            spinnerselecaodacidade.onItemSelectedListener = escolha
+
+            localizationSpinner(
+                mensagem,
+                janelacidadesview,
+                spinnerselecaodacidade,
+                spinnerselecaodobairro,
+                map
+            )
+
+
+        }
 
     }
 
@@ -100,24 +233,13 @@ class MapFeirasFragment : Fragment(), OnMapReadyCallback {
                         val longitude = it.longitude
                         userlocalization = LatLng(latitude, longitude)
 
-                        val cameraPosition = CameraPosition.Builder()
-                            .target(userlocalization)
-                            .zoom(13f)
-                            .build()
-
-                        map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
+                        camera.focusCamera(userlocalization, map)
                         map.addMarker(MarkerOptions().position(userlocalization).title("Marker"))
                     }
 
-                    binding.fabLocalization.setOnClickListener{
+                    binding.fabLocalization.setOnClickListener {
 
-                        val cameraPosition = CameraPosition.Builder()
-                            .target(userlocalization)
-                            .zoom(13f)
-                            .build()
-
-                        map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
-
+                        camera.focusCamera(userlocalization, map)
                     }
 
                 }
@@ -135,7 +257,7 @@ class MapFeirasFragment : Fragment(), OnMapReadyCallback {
         }
     }
 
-    private fun showFeirasLocalizationIn(map: GoogleMap){
+    private fun showFeirasLocalizationIn(map: GoogleMap) {
         val referenceListener = object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
 
@@ -143,7 +265,7 @@ class MapFeirasFragment : Fragment(), OnMapReadyCallback {
 
                     val feiras = feirasnapshot.getValue(FeirasModel::class.java)
                     feiras?.let {
-                        if(diasemana.trazSemana() == it.dia){
+                        if (diasemana.trazSemana()==it.dia.trim()) {
 
                             val latLng = LatLng(it.Latitude.toDouble(), it.Longitude.toDouble())
                             map.addMarker(
@@ -168,6 +290,46 @@ class MapFeirasFragment : Fragment(), OnMapReadyCallback {
         }
 
         database.addListenerForSingleValueEvent(referenceListener)
+    }
+
+    private fun geoLocalization(cidade: String, bairro: String, map: GoogleMap) {
+
+        val local = "$cidade,$bairro"
+        val gc = Geocoder(binding.root.context)
+        var list: List<Address?>? = null
+        try {
+            list = gc.getFromLocationName(local, 1)
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        val localization = Objects.requireNonNull<List<Address>>(list as List<Address>?)[0]
+
+        map.animateCamera(
+            CameraUpdateFactory.newCameraPosition(camera.focusCamera(localization))
+        )
+    }
+
+    private fun localizationSpinner(
+        mensagem: AlertDialog.Builder,
+        janelacidadesview: ViewGroup,
+        spinnerselecaodacidade: Spinner,
+        spinnerselecaodobairro: Spinner,
+        map: GoogleMap
+    ) {
+        mensagem.setTitle("Teste")
+        mensagem.setView(janelacidadesview)
+        mensagem.setPositiveButton(
+            "OK"
+        ) { _, _ ->
+
+            val spinnercidade = spinnerselecaodacidade.getSelectedItem().toString()
+            val spinnerbairro = spinnerselecaodobairro.getSelectedItem().toString()
+
+            geoLocalization(spinnercidade, spinnerbairro, map)
+
+        }
+        mensagem.setNegativeButton("Voltar") { dialog, which -> }
+        mensagem.show()
     }
 
 }
