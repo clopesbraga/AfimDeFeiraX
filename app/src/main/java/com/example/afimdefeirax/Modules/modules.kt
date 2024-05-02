cebc18d007f9d842b8188d9bf3c9c94891a5abfb
Cleiton Lopes Braga <clopesbraga@gmail.com>
2024-05-01 23:41:41 -0300

    refactor(TelaMapa):incluido classe FocusCamera no modulo do Koin.

--- app/src/main/java/com/example/afimdefeirax/Modules/modules.kt
+++ app/src/main/java/com/example/afimdefeirax/Modules/modules.kt
@@ -3,6 +3,7 @@ package com.example.afimdefeirax.Modules
 import android.app.Application
 import com.example.afimdefeirax.Repository.FeiraRepository.FeirasRepositoryImpl
 import com.example.afimdefeirax.Utils.DeviceCurrentTime
+import com.example.afimdefeirax.Utils.FocusCamera
 import com.example.afimdefeirax.Utils.LocationImpl
 import com.example.afimdefeirax.ViewModel.MapaFeirasViewModel
 import com.google.android.gms.location.FusedLocationProviderClient
@@ -15,6 +16,7 @@ val appModule = module{
      viewModel{ MapaFeirasViewModel() }
      single{FeirasRepositoryImpl()}
      single{LocationImpl(get(),get())}
+     single {FocusCamera()}
      single<FusedLocationProviderClient> {
         FusedLocationProviderClient(androidApplication())
     }
