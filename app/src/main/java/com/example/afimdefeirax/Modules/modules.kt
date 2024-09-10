ef324f8ea56fc02dc4c64c6b942f56823f9a38e3
Cleiton Lopes Braga <clopesbraga@gmail.com>
2024-09-10 00:10:06 -0300

    Refactor(TelaMapa): colocada funcoes na classe viewModel,

--- app/src/main/java/com/example/afimdefeirax/Modules/modules.kt
+++ app/src/main/java/com/example/afimdefeirax/Modules/modules.kt
@@ -4,20 +4,18 @@ import com.example.afimdefeirax.Repository.FeiraRepository.FeirasRepositoryImpl
 import com.example.afimdefeirax.Utils.FocusCamera
 import com.example.afimdefeirax.Utils.LocationImpl
 import com.example.afimdefeirax.ViewModel.MapaFeirasViewModel
-import com.google.android.gms.location.FusedLocationProviderClient
-import org.koin.android.ext.koin.androidApplication
+import com.google.android.gms.location.LocationServices
+import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module{
 
-     viewModel{ MapaFeirasViewModel() }
+     viewModel{ MapaFeirasViewModel(get()) }
      single{FeirasRepositoryImpl()}
-     single{LocationImpl(get())}
+     single {LocationServices.getFusedLocationProviderClient(androidContext()) }
+     single {LocationImpl(get())}
      single {FocusCamera()}
-     single<FusedLocationProviderClient> {
-        FusedLocationProviderClient(androidApplication())
-    }
  }
 
 
--- app/src/main/java/com/example/afimdefeirax/View/MapFeirasFragment.kt
+++ app/src/main/java/com/example/afimdefeirax/View/MapFeirasFragment.kt
@@ -1,11 +1,7 @@
 package com.example.afimdefeirax.View
 
-import android.Manifest
-import android.app.Activity
+
 import android.app.AlertDialog
-import android.content.pm.PackageManager
-import android.location.Address
-import android.location.Geocoder
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -13,39 +9,28 @@ import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.afimdefeirax.R
-import com.example.afimdefeirax.Repository.FeiraRepository.FeirasRepositoryImpl
-import com.example.afimdefeirax.Utils.FocusCamera
-import com.example.afimdefeirax.Utils.ILocation
-import com.example.afimdefeirax.Utils.LocationImpl
+import com.example.afimdefeirax.ViewModel.MapaFeirasViewModel
 import com.example.afimdefeirax.databinding.FragmentMapFeirasBinding
-import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.BitmapDescriptorFactory
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 import org.koin.android.ext.android.inject
-import java.io.IOException
-import java.util.Objects
 
 
 class MapFeirasFragment : Fragment(), OnMapReadyCallback {
 
     private var _binding: FragmentMapFeirasBinding? = null
     private val binding get() = _binding!!
-    private lateinit var userLocation: LatLng
 
-    private val camera : FocusCamera by inject()
-    private val feirasRepository : FeirasRepositoryImpl by inject()
-    private val locationProvider : LocationImpl by inject()
+    val viewModel: MapaFeirasViewModel by inject()
 
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View {
 
         _binding = FragmentMapFeirasBinding.inflate(inflater, container, false)
@@ -63,55 +48,14 @@ class MapFeirasFragment : Fragment(), OnMapReadyCallback {
 
     override fun onMapReady(map: GoogleMap): Unit = map.let {
 
-        showMyLocalizationIn(map)
-        showFeirasLocalizationIn(map)
+        viewModel.showMyLocalizationIn(map)
+        viewModel.showFeirasLocalizationIn(map)
         actionFloatButtons(map)
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
-    }
-
-    private fun showMyLocalizationIn(map: GoogleMap) {
-        when {
-            ContextCompat.checkSelfPermission(
-                binding.root.context as Activity,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED -> {
-
-                locationProvider.getLastLocation { location ->
-                    location?.let {
-
-                        userLocation = LatLng(it.latitude, it.longitude)
-                        camera.focusCamera(userLocation, map)
-                        map.addMarker(MarkerOptions().position(userLocation).title("Usuario"))
-                    }
-                }
-            }
-            else -> ActivityCompat.requestPermissions(
-                binding.root.context as Activity,
-                arrayOf(
-                    Manifest.permission.ACCESS_FINE_LOCATION,
-                    Manifest.permission.ACCESS_COARSE_LOCATION
-                ),
-                200
-            )
-        }
-    }
-    private fun showFeirasLocalizationIn(map: GoogleMap) {
-        feirasRepository.getFeirasLocations { feiras ->
-            feiras.forEach { feira ->
-                val latLng = LatLng(feira.Latitude.toDouble(), feira.Longitude.toDouble())
-                map.addMarker(
-                    MarkerOptions()
-                        .position(latLng)
-                        .title(feira.Feira)
-                        .snippet("${feira.endereco},${feira.bairro}")
-                        .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_loc))
-                )
-            }
-        }
     }
 
     private fun localizationSpinner(
@@ -126,39 +70,35 @@ class MapFeirasFragment : Fragment(), OnMapReadyCallback {
         mensagem.setPositiveButton(
             "OK"
         ) { _, _ ->
-            geoLocalization(
+            viewModel.geoLocalization(
                 spinnerselecaodacidade.getSelectedItem().toString(),
                 spinnerselecaodobairro.getSelectedItem().toString(),
-                map)
+                map
+            )
         }
         mensagem.setNegativeButton("Voltar") { dialog, which -> }
         mensagem.show()
     }
-    private fun geoLocalization(cidade: String, bairro: String, map: GoogleMap) {
 
-        val local = "$cidade,$bairro"
-        val gc = Geocoder(binding.root.context)
-        var list: List<Address?>? = null
-        try {
-            list = gc.getFromLocationName(local, 1)
-        } catch (e: IOException) {
-            e.printStackTrace()
-        }
-        val localization = Objects.requireNonNull<List<Address>>(list as List<Address>?)[0]
+    private fun actionFloatButtons(map: GoogleMap) {
 
-        map.animateCamera(
-            CameraUpdateFactory.newCameraPosition(camera.focusCamera(localization))
-        )
+        searchFeirasButton(map)
+        localizationUserButton(map)
+
     }
-    private fun actionFloatButtons(map : GoogleMap){
 
+    private fun localizationUserButton(map: GoogleMap) {
+        binding.fabLocalization.setOnClickListener {
+            viewModel.focusUserCamera(map)
+        }
+    }
+
+    private fun searchFeirasButton(map: GoogleMap) {
         binding.fabSearch.setOnClickListener {
 
             lateinit var bairrosadapter: ArrayAdapter<*>
             val inflater = requireActivity().layoutInflater
-            val mensagem = AlertDialog.Builder(
-                activity
-            )
+            val mensagem = AlertDialog.Builder(activity)
 
             val janelacidadesview =
                 inflater.inflate(R.layout.layout_list_cities_ditricts, null) as ViewGroup
@@ -173,9 +113,9 @@ class MapFeirasFragment : Fragment(), OnMapReadyCallback {
                 android.R.layout.simple_spinner_dropdown_item
             )
 
-            spinnerselecaodacidade.adapter=cidadesadapter
+            spinnerselecaodacidade.adapter = cidadesadapter
 
-            val  districtsarray = arrayOf(
+            val districtsarray = arrayOf(
 
                 R.array.sp_bairros,
                 R.array.guaru_bairros,
@@ -230,13 +170,7 @@ class MapFeirasFragment : Fragment(), OnMapReadyCallback {
                 map
             )
 
-
         }
-        binding.fabLocalization.setOnClickListener {
-
-            camera.focusCamera(userLocation, map)
-        }
-
     }
 
 }
