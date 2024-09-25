12f18c6e87a9e8b2b3291ac304fe8ce49bcdfedb
Cleiton Lopes Braga <clopesbraga@gmail.com>
2024-09-24 23:23:20 -0300

    Feature(TelaCompras): incluido selecionamento desabilitando o botao

--- app/src/main/java/com/example/afimdefeirax/View/ProdutosFragment.kt
+++ app/src/main/java/com/example/afimdefeirax/View/ProdutosFragment.kt
@@ -1,13 +1,16 @@
 package com.example.afimdefeirax.View
 
 
+
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -18,6 +21,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
@@ -25,8 +29,14 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
@@ -37,15 +47,11 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
 import com.example.afimdefeirax.Model.produtosList
-import com.example.afimdefeirax.ViewModel.ComprasViewModel
 import com.example.afimdefeirax.databinding.FragmentComprasBinding
 
 
+class ProdutosFragment : Fragment() {
 
-
-class ProdutosFragment : Fragment(){
-
-    private lateinit var _viewModel: ComprasViewModel
     private var _binding: FragmentComprasBinding? = null
     val binding get() = _binding!!
 
@@ -58,88 +64,113 @@ class ProdutosFragment : Fragment(){
 
         _binding = FragmentComprasBinding.inflate(inflater, container, false)
 
-        binding.composeView.setContent {
 
-            ListProdutos()
+        binding.composeViewCompras.setContent {
+
+            Column {
+
+                ListProdutos()
+                binding.fabCompras
+
+            }
         }
 
         return binding.root
     }
 
-    @OptIn(ExperimentalMaterial3Api::class)
-    @Composable
-    fun ListProdutos() {
+}
 
-        Scaffold(
-            topBar = {
-                TopAppBar(
-                    colors = topAppBarColors(
-                        containerColor = Color(0xFF009688)
-                    ),
-                    title = {
-                        Row(
-                            modifier = Modifier.fillMaxWidth(),
-                            horizontalArrangement = Arrangement.Center
-                        ) {
-                            Text(text = "Compras", color = Color.White)
-                        }
-                    },
-                )
-            }
-        ) { innerpadding ->
 
-            LazyColumn(
-                modifier = Modifier
-                    .background(Color(0xFF009688))
-                    .padding(innerpadding)
-                    .fillMaxSize()
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ListProdutos() {
 
-            ) {
-                items(produtosList.size) { produtos ->
-                    Column {
-                        Text(
-                            text = produtosList[produtos][0].name,
-                            color = Color.White,
-                            fontSize = 20.sp,
-                            fontWeight = FontWeight.Bold,
-                            textDecoration = TextDecoration.Underline
-                        )
-                        LazyRow {
-                            items(produtosList[produtos].size) { items ->
-                                Column(
-                                    horizontalAlignment = Alignment.CenterHorizontally
-                                ){
-                                    Text(produtosList[produtos][items].name)
-                                    Card(
-                                        modifier = Modifier
-                                            .padding(8.dp)
-                                            .size(100.dp)
-                                            .clickable {  }
-                                    ){
-                                        Image(
-                                            painter = painterResource(
-                                                id = produtosList[produtos][items].imageResId),
-                                            contentDescription = null,
-                                            contentScale = ContentScale.Crop,
-                                            modifier = Modifier
-                                                .size(100.dp)
-                                                .clip(CircleShape)
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = topAppBarColors(
+                    containerColor = Color(0xFF009688)
+                ),
+                title = {
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        Text(text = "Compras", color = Color.White)
+                    }
+                },
+            )
+        }
+    ) { innerpadding ->
+
+        LazyColumn(
+            modifier = Modifier
+                .background(Color(0xFF009688))
+                .padding(innerpadding)
+                .fillMaxSize()
+
+        ) {
+            items(produtosList.size) { produtos ->
+                var focusedStates = remember {
+                    mutableStateListOf(*List(produtosList[produtos].size) { true }.toTypedArray())
+                }
+                Column {
+                    Text(
+                        text = produtosList[produtos][0].name,
+                        color = Color.White,
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Bold,
+                        textDecoration = TextDecoration.Underline
+                    )
+                    LazyRow {
+                        items(produtosList[produtos].size) { items ->
+                            Column(
+                                horizontalAlignment = Alignment.CenterHorizontally
+                            ) {
+                                Text(produtosList[produtos][items].name)
+                                Card(
+                                    modifier = Modifier
+                                        .padding(8.dp)
+                                        .size(100.dp)
+                                        .clickable(enabled=focusedStates[items]) {
+
+                                                focusedStates[items] = false
+
+
+                                        }
+                                        .focusable(enabled = true)
+                                        .border(
+                                            width = if (!focusedStates[items]) 5.dp else 0.dp,
+                                            color = if (!focusedStates[items]) Color.Green else Color.Transparent,
+                                            shape = RoundedCornerShape(8.dp)
                                         )
+                                ) {
+                                    Image(
+                                        painter = painterResource(
+                                            id = produtosList[produtos][items].imageResId
+                                        ),
+                                        contentDescription = null,
+                                        contentScale = ContentScale.Crop,
+                                        modifier = Modifier
+                                            .size(100.dp)
+                                            .clip(CircleShape)
+                                            .alpha(if (focusedStates[items]) 1f else 0.5f)
+                                    )
 
-                                    }
                                 }
-
                             }
 
                         }
 
                     }
-                }
 
+                }
             }
+
 
         }
 
     }
 
 }
+
