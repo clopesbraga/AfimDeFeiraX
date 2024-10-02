0e2dfc88c6153c29fb9f1c5e3251299de10ca57a
Cleiton Lopes Braga <clopesbraga@gmail.com>
2024-10-02 00:19:41 -0300

    Refactor(modules): incluido no módulo ProdutosViewModel e ListProdutosShared

--- app/src/main/java/com/example/afimdefeirax/Modules/modules.kt
+++ app/src/main/java/com/example/afimdefeirax/Modules/modules.kt
@@ -1,9 +1,11 @@
 package com.example.afimdefeirax.Modules
 
 import com.example.afimdefeirax.Repository.FeiraRepository.FeirasRepositoryImpl
+import com.example.afimdefeirax.SharedPreferences.ListProdutosShared
 import com.example.afimdefeirax.Utils.FocusCamera
 import com.example.afimdefeirax.Utils.LocationImpl
 import com.example.afimdefeirax.ViewModel.MapaFeirasViewModel
+import com.example.afimdefeirax.ViewModel.ProdutosViewModel
 import com.google.android.gms.location.LocationServices
 import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModel
@@ -12,10 +14,12 @@ import org.koin.dsl.module
 val appModule = module{
 
      viewModel{ MapaFeirasViewModel(get()) }
+     viewModel{ ProdutosViewModel(get()) }
      single{FeirasRepositoryImpl()}
      single {LocationServices.getFusedLocationProviderClient(androidContext()) }
      single {LocationImpl(get())}
      single {FocusCamera()}
+     single { ListProdutosShared(androidContext()) }
  }
 
 
