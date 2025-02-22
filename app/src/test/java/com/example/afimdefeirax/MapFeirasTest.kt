0b8f9477f96d42659d7113f9d1f20ec0da51f05d
clopesbraga <clopesbraga@gmail.com>
2025-02-21 23:08:23 -0300

    feature  (MapFeirasTest): terminando de fazer os testes unitarios do state.

--- app/src/test/java/com/example/afimdefeirax/MapFeirasTest.kt
+++ app/src/test/java/com/example/afimdefeirax/MapFeirasTest.kt
@@ -21,6 +21,7 @@ import kotlinx.coroutines.test.TestScope
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
+import org.junit.After
 import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
@@ -162,6 +163,9 @@ class MapFeirasTest {
 
     }
 
-
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain()
+    }
 
 }
