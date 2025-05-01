f64d067f3cb0594d0e0b49f7d97e24d65bd00d9d
clopesbraga <clopesbraga@gmail.com>
2025-05-01 00:24:01 -0300

    fix(MapaFeiraViewModel): inclusao tagueamento em erro de não permitir localizacao

--- app/src/main/java/com/example/afimdefeirax/Utils/Monitoring.kt
+++ app/src/main/java/com/example/afimdefeirax/Utils/Monitoring.kt
@@ -43,6 +43,7 @@ object Monitoring {
         const val SHOW_MENU_NEIGHBORS = MAP_SCREEN + "_show_menu"
         const val CITY_SELECTED = MAP_SCREEN + "_city_" + Actions.SELECTED
         const val NEIGHBOR_SELECTED = MAP_SCREEN + "_neighbor_" + Actions.SELECTED
+        const val MAP_ERROR = MAP_SCREEN + "_error_starts_map"
     }
 
     object Product {
