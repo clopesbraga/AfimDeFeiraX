30189acaf790752fc1523110cdbe569fcd57ddc7
clopesbraga <clopesbraga@gmail.com>
2025-05-01 22:26:14 -0300

    update(Monitorin): atualizado taguamento com MAP_SHOW_NEIGHBOOR_ERROR_

--- app/src/main/java/com/example/afimdefeirax/Utils/Monitoring.kt
+++ app/src/main/java/com/example/afimdefeirax/Utils/Monitoring.kt
@@ -44,6 +44,7 @@ object Monitoring {
         const val CITY_SELECTED = MAP_SCREEN + "_city_" + Actions.SELECTED
         const val NEIGHBOR_SELECTED = MAP_SCREEN + "_neighbor_" + Actions.SELECTED
         const val MAP_ERROR = MAP_SCREEN + "_error_starts_map"
+        const val MAP_SHOW_NEIGHBOOR_ERROR_ = MAP_SCREEN + "_error_show_neighboor"
     }
 
     object Product {
