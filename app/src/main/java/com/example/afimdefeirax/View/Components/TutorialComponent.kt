3ea0fdf0741324f51ad2838bc40e3ff43b95d354
clopesbraga <clopesbraga@gmail.com>
2025-06-21 00:05:17 -0300

    update(Tutorial):configurando tutorial component

--- app/src/main/java/com/example/afimdefeirax/View/Components/TutorialComponent.kt
+++ app/src/main/java/com/example/afimdefeirax/View/Components/TutorialComponent.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.canopas.lib.showcase.IntroShowcase
 import com.canopas.lib.showcase.component.ShowcaseStyle
+import com.canopas.lib.showcase.component.rememberIntroShowcaseState
 
 @Composable
 fun TutorialShowCaseComponent(
@@ -26,42 +27,44 @@ fun TutorialShowCaseComponent(
     title: String,
     description: String,
     showintro: Boolean,
-    onTutorialCompleted: Boolean,
-    content: @Composable () -> Unit
+    onTutorialCompleted: () -> Unit,
+    content: @Composable (() -> Unit)
 
 ) {
 
     IntroShowcase(
         showIntroShowCase = showintro,
         dismissOnClickOutside = true,
-        onShowCaseCompleted = { true },
+        onShowCaseCompleted = {
+            onTutorialCompleted()
+        },
     ) {
 
         Box(
             modifier = Modifier
                 .padding(16.dp)
                 .introShowCaseTarget(
-                index = targetIndex,
-                style = tutorialStyle,
-                content = {
-                    Column {
-                        Text(
-                            text = title,
-                            color = Color.White,
-                            fontSize = 24.sp,
-                            fontWeight = FontWeight.Bold
-                        )
-                        Spacer(modifier = Modifier.height(4.dp))
-                        Text(
-                            text = description,
-                            color = Color.White,
-                            fontSize = 16.sp
-                        )
-                        Spacer(modifier = Modifier.height(10.dp))
+                    index = targetIndex,
+                    style = tutorialStyle,
+                    content = {
+                        Column {
+                            Text(
+                                text = title,
+                                color = Color.White,
+                                fontSize = 24.sp,
+                                fontWeight = FontWeight.Bold
+                            )
+                            Spacer(modifier = Modifier.height(4.dp))
+                            Text(
+                                text = description,
+                                color = Color.White,
+                                fontSize = 16.sp
+                            )
+                            Spacer(modifier = Modifier.height(10.dp))
+                        }
                     }
-                }
-            ),
-        ){
+                ),
+        ) {
             content()
         }
 
