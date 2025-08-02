be251c85fa80478ad98826b55ea00597787262a2
clopesbraga <clopesbraga@gmail.com>
2025-08-01 23:35:20 -0300

    bugfix(proguard) : mudado o caminho de com.example para com.branchh

--- app/src/main/java/com/example/afimdefeirax/Utils/Location/ILocation.kt
+++
@@ -1,9 +0,0 @@
-package com.example.afimdefeirax.Utils.Location
-
-import android.location.Location
-import com.google.android.gms.maps.model.LatLng
-
-interface ILocation {
-    fun getLastLocation(callback: (Location?) -> Unit)
-
-}
